e63084c1fb11393049b7370f9c72b4dbc0909351
Erik Brendel <erik.brendel@gmx.de>
2018-01-04 14:03:48 +0100

    click command

---
+++ installOpenSSL.sh
@@ -0,0 +1,10 @@
+sudo apt install wget
+wget https://www.openssl.org/source/openssl-1.0.2-latest.tar.gz
+tar -zxf openssl-1.0.2-latest.tar.gz
+cd openssl-1.0.2l
+./config
+make
+make test
+sudo make install
+sudo mv /usr/bin/openssl /root/ && sudo ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+openssl version
