9c3873b8d82f6a55554cdc5dfc0d6cac7e85d1de
Erik Brendel <erik.brendel@gmx.de>
2018-01-08 15:54:52 +0100

    Update openssl installer

--- installOpenSSL.sh
+++ installOpenSSL.sh
@@ -1,7 +1,7 @@
 sudo apt install wget
 wget https://www.openssl.org/source/openssl-1.0.2-latest.tar.gz
 tar -zxf openssl-1.0.2-latest.tar.gz
-cd openssl-1.0.2l
+cd openssl-1.0.2n
 ./config
 make
 make test
